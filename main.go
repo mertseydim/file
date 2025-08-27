e3e17eaee98e085a0d427fb2045406c44d99f02b
Your Name <you@example.com>
2025-08-27 12:27:54 +0300

    ttt

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func initDB() {
 		User:   "root",
 		Passwd: "190732Mert",
 		Net:    "tcp",
-		Addr:   "167.172.177.65",
+		Addr:   "127.0.0.1:3306",
 		DBName: "file_transfer_db",
 	}
 	var err error
