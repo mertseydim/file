5f9fe73d7cf0dd4f16286f580053d4c84ac4f8da
Your Name <you@example.com>
2025-08-28 11:39:17 +0300

    ffff

--- main.go
+++ main.go
@@ -103,7 +103,7 @@ func downloadFile(c *fiber.Ctx) error {
 }
 
 func sendEmail(to, from, filename, link string) {
-	apiKey := os.Getenv("SENDGRID_API_KEY")
+	apiKey := os.Getenv("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")
 	if apiKey == "" {
 		log.Println("SENDGRID_API_KEY ortam değişkeni ayarlanmadı")
 		return
